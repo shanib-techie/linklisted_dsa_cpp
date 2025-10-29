d259b6f3043aeb8744d916bd610a3896d30e653c
shanib-techie <shanibkhan622@gmail.com>
2025-10-29 11:49:30 +0530

    yoyo

---
+++ link_lised_basic.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* next;
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void display_by_loop(Node* head){
+        Node* temp = head;
+while(temp!=NULL){
+    cout<<temp->val<<" ";
+    temp = temp->next;
+}
+cout<<"\n";
+}
+ int size(Node* head ){
+    Node* temp =head;
+    int n = 0;
+    while(temp!=NULL){
+        n++;
+        temp = temp->next;
+    }
+    return n;
+ }
+int main(){
+    Node* a = new Node(10) ;
+    Node* b = new Node(20);
+    Node* c = new Node(30);
+    Node* d = new Node(40);
+    a->next = b;
+    b->next = c;
+    c->next =d;
+    display_by_loop(a);
+    cout<<size(a);
+
+}
