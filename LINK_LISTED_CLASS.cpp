137e384894f78e144c1806d0d0467f0f0d099532
shanib-techie <shanibkhan622@gmail.com>
2025-11-27 10:20:29 +0530

    add

--- LINK_LISTED_CLASS.cpp
+++ LINK_LISTED_CLASS.cpp
@@ -22,7 +22,7 @@ class LinkedList{  //user defined data structure
         this->tail = NULL;
     }
 
-    void insertatEnd(int val){
+    void insertattail(int val){
         Node* temp = new Node(val);
         if(size==0){
             head = tail = temp;
@@ -33,7 +33,109 @@ class LinkedList{  //user defined data structure
         }
         size++;
     }
+    void insertathead(int val){
+        Node* temp = new Node(val);
+        temp->next = head;
+        head = temp;
+    }
+    void insertatidx(int idx,int val){
+        if(idx == 0){
+            insertathead(val);
+        }else if (idx == size-1)
+        {
+            insertattail(val);
+        }else if (idx < 0 || idx > size){
+            cout<<"invalid";
+        }else{
+            Node* t = new Node(val);
+            Node* temp = head;
+            for(int i = 1; i<=idx-1;i++){
+                temp = temp->next;
+            }
+            t->next= temp->next;
+            temp->next =t;
+        }
+    }
 
+
+
+
+    int getatidx(int idx){
+        if(idx < 0 || idx >= size){
+            cout<<"invalid....";
+            return -1;
+
+        }
+        else if(idx == 0){
+            head->val;
+        }
+        else if(idx == size-1){
+            return tail->val;
+        }
+        else{
+        Node* temp = head;
+        for(int i = 1 ; i<= idx-1;i++){
+            temp = temp->next;
+        }
+        return temp->val;
+    }}
+
+
+
+    void del_at_head(){
+
+
+        if(size == 0){
+            cout<<"list is empty";
+        }else if (size == 1){
+            head = tail = NULL;
+            size --;
+        }
+        else{
+            head = head->next;
+            size--;
+        }
+
+    }
+    void del_at_tail(){
+             if(size == 0){
+            cout<<"list is empty";
+        }else if (size == 1){
+            head = tail = NULL;
+            size --;
+        }else{
+            Node* temp = head;
+            while (temp->next->next != NULL) //10 20 30 40 50 null
+            {
+                /* code */
+                temp = temp->next;
+            }
+            temp->next = NULL;
+            size--;
+            
+        }
+
+    }
+    void del_at_idx(int idx){
+                if(idx < 0 || idx >= size){
+            cout<<"invalid....";
+        }
+        else if (idx == 0){
+            del_at_head();
+        }
+        else if (idx == size-1){
+            del_at_tail();
+        }
+        else{
+            Node* temp = head;
+            for(int i = 1;i<=idx-1;i++){
+                temp= temp->next;
+            }
+            temp->next = temp->next->next;
+        }
+        
+
+    }
     void display(){
         Node* temp = head;
         while(temp!=NULL){
@@ -42,6 +144,9 @@ class LinkedList{  //user defined data structure
         }
         cout<<endl;
     }
+
+
+
     int size_of_LL(){
         size = 0;
         Node* temp = head;
@@ -57,11 +162,18 @@ class LinkedList{  //user defined data structure
 
 int main(){
     LinkedList ll;
-    ll.insertatEnd(10);
-    ll.insertatEnd(20);
-    ll.insertatEnd(40);
+    ll.insertattail(10);
+    ll.insertattail(20);
+    ll.insertattail(40);
     cout<<"\n"<<" ";
     cout<<ll.size_of_LL();
     ll.display();
-
+    ll.insertathead(50);
+       ll.display();
+    // ll.insertatidx(54,30);
+    cout<<ll.getatidx(2);
+    ll.insertathead(45);
+    ll.insertattail(34);
+    ll.del_at_head();
+    ll.display();
 }
